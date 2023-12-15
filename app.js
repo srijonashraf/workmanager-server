453cd3e76d69d0158771b32a4dd7a06074d56245
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-15 14:40:38 +0600

    cors added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const jsonwebtoken = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const nodeMailer = require("nodemailer");
 const app = express();
+const cors =require('cors');
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
@@ -11,7 +12,7 @@ require("dotenv").config();
 // Use body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 

591d8267d7e78c9e0c20250bd1090e1a4f3a8bda
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-18 13:07:19 +0600

    comment removed

--- src/middleware/AuthVerifyMiddleware.js
+++ src/middleware/AuthVerifyMiddleware.js
@@ -1,21 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// module.exports=(req,res,next)=>{
-//     let Token=req.headers['token'];
-//     jwt.verify(Token,"ABC-123",function (err,decoded) {
-//         if(err){
-//             console.log(Token)
-//             res.status(401).json({status:"unauthorized"})
-//         }
-//         else {
-//             let email=decoded['data'];
-//             // console.log(email)
-//             req.headers.email=email //It will auto set the email into the header
-//             next();
-//         }
-//     })
-// }
-
-
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {

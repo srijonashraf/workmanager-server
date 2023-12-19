7a4a80d0f9a6beb333f7696e7fc933d24cc58917
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-19 23:21:17 +0600

    cors updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ require("dotenv").config();
 // Use body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 

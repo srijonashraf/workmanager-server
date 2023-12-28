55579b405e4f15deae7fbc3266314520b3e66491
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-29 02:54:44 +0600

    no modification

--- src/helper/CreateJWTTokenHelper.js
+++ src/helper/CreateJWTTokenHelper.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 const CreateJWTToken = (reqBody) => {
   let Payload = {
-    // exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
     exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60, // 1 day expiration
     data: reqBody["email"],
   };

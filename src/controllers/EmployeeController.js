679b246a74ee5a1a5f1e9e0cdc7da7c2a4d179c4
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-05-01 01:32:14 +0600

    deleted sameSite

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -36,7 +36,7 @@ exports.UserLogin = async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: false, // Ensures that the cookie is only sent over HTTPS
-        sameSite: "Lax", // Allows the cookie to be sent in cross-origin requests; Commenting this is working on localhost
+        // sameSite: "Lax", // Allows the cookie to be sent in cross-origin requests; Commenting this is working on localhost
         maxAge: 24 * 60 * 60 * 1000,
       });
 

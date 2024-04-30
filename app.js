53af95593e51c2a88f22f4174b1bd613ce9ec3ba
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-04-30 23:42:09 +0600

    added origin inside the cors

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ require("dotenv").config();
 // Use body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({ origin: "*", credentials: true }));
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://workmanager-srijonashraf.netlify.app",
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 
